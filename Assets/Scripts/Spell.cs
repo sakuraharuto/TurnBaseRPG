c1c7e9c6afbb6e78eb456cfdcd7032bf96cbfbb4
Ruocheng "Sam" Yaoxi <88380883+sakuraharuto@users.noreply.github.com>
2022-02-16 15:35:49 -0500

    Spell setup

---
+++ Assets/Scripts/Spell.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spell : MonoBehaviour
+{
+    public string spellName;
+    public int power;
+    public int manaCost;
+    public enum SpellType{ Attack, Heal}
+    public SpellType spellType;
+
+    private Vector3 targetPosition; 
+
+    private void Update() {
+        if(targetPosition != Vector3.zero){   // There is a target
+            transform.position = Vector3.MoveTowards(transform.position, targetPosition, 20f);   //From spell pos to target pos in speed 20f
+            if(Vector3.Distance(transform.position, targetPosition) < 0.25){
+                Destroy(this.gameObject, 1);
+            }
+        } else { // There is no target
+            Destroy(this.gameObject);
+        }
+    }
+    public void CastSpell(GameObject target){
+        targetPosition = target.transform.position;
+
+        if (spellType == SpellType.Attack){
+            
+        } else if (spellType == SpellType.Heal){
+
+        }
+    }
+}
