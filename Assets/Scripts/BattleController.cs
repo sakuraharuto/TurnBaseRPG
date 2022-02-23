0be75fb6d116a09f150f65898fd40e16ceea2b55
Ruocheng "Sam" Yaoxi <88380883+sakuraharuto@users.noreply.github.com>
2022-02-23 18:09:56 -0500

    Battle UI controller

--- Assets/Scripts/BattleController.cs
+++ Assets/Scripts/BattleController.cs
@@ -5,12 +5,13 @@ using UnityEngine;
 public class BattleController : MonoBehaviour
 {   
     public static BattleController Instance {get; set;}
-    public Dictionary<int, List<Character>> characters = new Dictionary<int, List<Character>>();  // 0 for player, 1 for enemy
+    public Dictionary<int, List<Character>> characters = new Dictionary<int, List<Character>>();  // 0 for player, 1 for enemy. For example, characters[1][0] means first character on enemy side
     public int characterTurnIndex;
     public int activeTurn;
     public Spell playerSelectedSpell;
     public bool playerIsAttack;
     [SerializeField] private BattleSpwanPoint[] spwanPoints;
+    [SerializeField] private BattleUIController uiController;
 
     private void Start() {
         if (Instance != null && Instance != this){
@@ -38,7 +39,8 @@ public class BattleController : MonoBehaviour
     }
 
     private void nextTurn(){
-        activeTurn = activeTurn == 0 ? 1 : 0;  // If we are in turn 0, set activeTurn to 1. If its not 0, which means we are in turn 1, set activeTurn to 0
+        // If we are in turn 0, set activeTurn to 1. If its not 0, which means we are in turn 1, set activeTurn to 0. This means after we moved, enemies move, than we move again.
+        activeTurn = activeTurn == 0 ? 1 : 0;  
     }
 
     private void nextAct(){
@@ -53,33 +55,38 @@ public class BattleController : MonoBehaviour
 
             switch(activeTurn){
                 case 0:
-                // UI stuff
+                // Player team move, update UI
+                uiController.ToggleActionState(true);
+                uiController.BuildSpellList(GetCurrentCharacter().spells);
                 break;
 
                 case 1:
-                //UI stuff and action
+                // Enemy team move, update UI
                 StartCoroutine(PerformAct());
+                uiController.ToggleActionState(false);
                 break;
             }
         } else {
-            Debug.Log("Battle finish"); // One side all dead, back to town scene maybe? 
+            Debug.Log("Battle finish"); // Battle finished, elimated all enemies or we all dead, back to town scene or gameover maybe? 
         }
     }
 
     IEnumerator PerformAct(){
         yield return new WaitForSeconds(.75f);
-        if(characters[activeTurn][characterTurnIndex].health > 0){
-            characters[activeTurn][characterTurnIndex].GetComponent<Enemy>().Act();
+        if(GetCurrentCharacter().health > 0){
+            GetCurrentCharacter().GetComponent<Enemy>().Act();
         }
+        uiController.UpdateCharacterUI();
         yield return new WaitForSeconds(1f);
         nextAct();
     }
 
     public void SelectCharacter(Character character){
         if(playerIsAttack){
-            DoAttack(characters[activeTurn][characterTurnIndex], character);
+            DoAttack(GetCurrentCharacter(), character);
         } else if(playerSelectedSpell != null){
-            if(characters[activeTurn][characterTurnIndex].CastSpell(playerSelectedSpell, character)){
+            if(GetCurrentCharacter().CastSpell(playerSelectedSpell, character)){
+                uiController.UpdateCharacterUI();
                 nextAct();
             } else {
                 Debug.LogWarning("Not enough mana to cast spell");
@@ -98,5 +105,9 @@ public class BattleController : MonoBehaviour
         for(int i = 0; i < enemies.Count; i++){ 
             characters[1].Add(spwanPoints[i].Spwan(enemies[i]));  // Spwan point(0,1,2) belong to enemies
         }
+    }
+
+    public Character GetCurrentCharacter(){
+        return characters[activeTurn][characterTurnIndex]; // Return the character that currently moving
     }
 }
